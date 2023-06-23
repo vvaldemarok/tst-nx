d6779a9add502ee03b5e7dcda36c6d5d4e0297a9
Dmitry Gaiiulin <ft-dmitry@mail.ru>
2023-06-23 17:15:21 +0500

    feat(first-app): added new text component

--- packages/first-app/App.tsx
+++ packages/first-app/App.tsx
@@ -34,7 +34,7 @@ function Section({children, title}: SectionProps): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
-      {/* <Text
+      <Text
         style={[
           styles.sectionTitle,
           {
@@ -42,7 +42,17 @@ function Section({children, title}: SectionProps): JSX.Element {
           },
         ]}>
         {title}
-      </Text> */}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+        fix
+      </Text>
       <Text
         style={[
           styles.sectionDescription,

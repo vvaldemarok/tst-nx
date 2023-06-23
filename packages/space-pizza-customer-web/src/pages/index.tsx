d7ae23339478c3093fa8385a149a9b317cacddf5
vvaldemarok <v.efimov@jingu.ru>
2023-06-23 19:57:43 +0600

    feat(height): changed

--- packages/space-pizza-customer-web/src/pages/index.tsx
+++ packages/space-pizza-customer-web/src/pages/index.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             src="/next.svg"
             alt="Next.js Logo"
             width={180}
-            height={37}
+            height={38}
             priority
           />
         </div>
